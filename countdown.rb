f68a47c4a6229b9d052147ab91f99468c44917b3
Cheryl Jensen <cherylbjensen@gmail.com>
2020-07-12 04:33:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
 
-def countdown
+
+def countdown(number)
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 0
+  end
+puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (sleep)
+  sleep (1)
+  while number > 0
+  number -= 0
+end
 end
